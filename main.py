7d4f536e26ee9f47a80e27f0581129ec107b7ac2
zyed07 <sayedehthisham21@icet.ac.in>
2024-08-26 22:13:46 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+import pyttsx3
+if __name__ == '__main__':
+    text_to_speech = pyttsx3.init()
+    while(True):
+        word = input("Enter your command: ")
+        if word == '0':
+            break
+        text_to_speech.say(word)
+        text_to_speech.runAndWait()
